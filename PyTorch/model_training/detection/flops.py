e2ff7466027c076004e4280e478ea7f2af0475ce
okupyn <orestkupyn@gmail.com>
2019-08-02 21:40:09 +0300

    added flops calculator

---
+++ PyTorch/model_training/detection/flops.py
@@ -0,0 +1,30 @@
+import cv2
+import torch
+from thop import profile, clever_format
+
+from model_training.detection.ssd import build_ssd
+from model_training.detection.retinanet import build_retinanet
+from model_training.detection.config import get_config
+
+cv2.setNumThreads(0)
+
+
+def _get_model(config):
+    model_config = config['model']
+    if model_config['name'] == 'ssd':
+        model = build_ssd(model_config)
+    elif model_config['name'] == 'retina_net':
+        model = build_retinanet(model_config)
+    else:
+        raise ValueError("Model [%s] not recognized." % model_config['name'])
+    return model
+
+
+if __name__ == '__main__':
+    config = get_config("config/train.yaml")
+    model = _get_model(config)
+    input = torch.randn(1, 3, 256, 256)
+    flops, params = profile(model, inputs=(input,))
+    flops, params = clever_format([flops, params], "%.3f")
+    print('Flops - {}'.format(flops))
+    print('Params - {}'.format(params))
